a531d991b6f8e9b8013505a80bccf9cb2fbc0904
박진영 <tear4321@cau.ac.kr>
2023-11-24 18:07:23 +0900

    feature/lose point when out of terrain
    rigid body, Collider

--- Assets/Scripts/GameStageController.cs
+++ Assets/Scripts/GameStageController.cs
@@ -1,3 +1,5 @@
+using System;
+using Models;
 using UnityEngine;
 
 public class GameStageController : MonoBehaviour
@@ -6,11 +8,37 @@ public class GameStageController : MonoBehaviour
     public GameObject enemyObject;
     public static float offsetF => 0.5f;
 
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.GetInstance();
+    }
 
     public void InitStage()
     {
         var stageMid = transform.position;
         playerObject.transform.position = stageMid - (transform.forward* 1.0f);
         enemyObject.transform.position = stageMid + (transform.forward* 1.0f);
+    }
+
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log($"{other.gameObject.tag} is out");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"{other.gameObject.tag} is out - trigger");
+
+        if (other.CompareTag("Player"))
+        {
+            gameManager.LosePointTo(CharacterType.Player);
+        }
+        else if (other.CompareTag("Enemy"))
+        {
+            gameManager.LosePointTo(CharacterType.Enemy);
+        }
     }
 }
