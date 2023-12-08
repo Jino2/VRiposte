5da01905e3e3a2fddf45d0856e55c17c130952fb
heejae <linkyouhj@gmail.com>
2023-12-08 22:15:00 +0900

    공중부양 해결, collider gravity X, animation 수정

--- Assets/Scripts/EnemyAction.cs
+++ Assets/Scripts/EnemyAction.cs
@@ -8,11 +8,10 @@ public class EnemyAction : MonoBehaviour
     public Transform playerTransform;
     public float moveSpeed = 0.5f;
     public float closeDistance = 2.5f;
-
+    public float attackDistance = 2.0f;
     private int currentAction = -1;
     private int lastAction = -1; // 마지막 액션 추적
     private bool isActionCompleted = true;
-    private int retreatFrameCounter = 0; // 후퇴 프레임 카운터
 
     void Start()
     {
@@ -28,63 +27,54 @@ public class EnemyAction : MonoBehaviour
         // 현재 애니메이션이 끝났는지 확인
         if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
         {
-            isActionCompleted = true;
+            if (!isActionCompleted)
+            {
+                isActionCompleted = true;
+                ChooseNewAction();
+            }
         }
 
-        if (distanceToPlayer > closeDistance)
+        Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        directionToPlayer.y = 0;
+        if (distanceToPlayer > closeDistance||currentAction==0)
         {
             SetAction(0); // 이동 상태 설정
+            transform.position += directionToPlayer * moveSpeed * Time.deltaTime;
         }
-        else if(isActionCompleted)
+        if (currentAction == 2)
         {
-            if(currentAction == 1) // attack 후에는 retreat 선택
-            {
-                SetAction(3);
-            }
-            else
-            {
-                int newAction;
-                do
-                {
-                    newAction = Random.Range(1, 3); // 같은 액션을 연속해서 취하지 않도록 랜덤 선택
-                } while (newAction == lastAction);
-                
-                SetAction(newAction);
-            }
+            transform.position -= directionToPlayer * moveSpeed * Time.deltaTime; // 후퇴
         }
-
-        if(currentAction == 3 && isActionCompleted)
-        {
-            if (retreatFrameCounter < 30)
-            {
-                Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
-                transform.position -= directionToPlayer * moveSpeed * Time.deltaTime; // 후퇴
-                retreatFrameCounter++;
-            }
-            else
-            {
-                retreatFrameCounter = 0; // 카운터 초기화
-                isActionCompleted = true; // 후퇴 완료
-            }
-        }
-        else
-        {
-            if (currentAction == 0 && isActionCompleted) // 이동
-            {
-                Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
-                transform.position += directionToPlayer * moveSpeed * Time.deltaTime;
-            }
+        if (distanceToPlayer<attackDistance){
+            SetAction(1);
         }
     }
 
+    void ChooseNewAction()
+    {
+        int newAction;
+        do
+        {
+            newAction = Random.Range(1, 3); // 1부터 3까지 랜덤 선택 (attack, dodge, retreat)
+        } while (newAction == lastAction);
+
+        SetAction(newAction);
+    }
+    // void PerformCurrentAction(float distanceToPlayer)
+    // {
+
+    //     // 추가적인 액션 처리가 필요하면 여기에 코드를 추가합니다.
+    // }
+
     void SetAction(int action)
     {
-        if (action != currentAction && isActionCompleted && action != lastAction)
+        if (action != currentAction && isActionCompleted)
         {
             animator.SetBool("isWalking", action == 0);
             animator.SetBool("attack", action == 1);
             animator.SetBool("retreat", action == 2);
             animator.SetBool("dodge", action == 3);
+
             lastAction = currentAction;
             currentAction = action;
             isActionCompleted = false;
